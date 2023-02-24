1cd03576d71d08d62df3fce27549fdbf47f38e6b
Hieu Cu <hieu.dcu@gmail.com>
2023-02-24 18:19:35 +1300

    Added clear input function after calculation when user click any new input.

--- script.js
+++ script.js
@@ -3,60 +3,61 @@ const userInput = { // for comparing with inputs.
     operators: ["+", "-", "x", "÷"],
     result: "=",
 }
-let displayArray = [];//empty arrays to catch all value instances of the pressed number button.
+let displayArray = [];// empty arrays to catch all value instances of the pressed number button.
 let jointInput1;// join elements in above array into string as full number.
 let jointInput2;
-//Input display
 let input1 = null; // user input values are stored here.
-let input2 = null;
+let input2 = null; // second input store here after user click equal button.
 let displayDigit;
+let equationResult = null;
 const topScreen = document.querySelector(".screenTop");
 const digitInputs = document.querySelectorAll('.num'); // access all class "num"
     digitInputs.forEach(digitInput => { // loop through "num" button to add click event listener.
-        digitInput.addEventListener('click', event1 => {
+        digitInput.addEventListener('click', event1 => {          
             const inputDigit = event1.target.innerHTML;
             displayDigit = document.createTextNode(inputDigit); // create a text node to append it to the first digit displayed.
             topScreen.appendChild(displayDigit);
-            displayArray.push(inputDigit);//push all instances of input1 into array.
-            });
-        });
-//Operators function to calculate
-let equationResult = null;
-let operatorInput = document.querySelectorAll(".ope");
-    operatorInput.forEach(operator => {
-        operator.addEventListener('click', (event) => {//only return input1 once an operator is clicked.
-            operatorInput = event.target.innerHTML; //access the symbols.
-            displayOp = document.createTextNode(operatorInput);
-            topScreen.appendChild(displayOp);
-            jointInput1 = displayArray.join('');//join elements inside array into string
-            console.log('first input: '+jointInput1);
-            input1 = parseInt(jointInput1);//assign joint string into input1 parsed as integer.
-            console.log(operatorInput);
-            displayArray = []; //empty array after operator clicked, ready to take value of input 2
+            displayArray.push(inputDigit);// push all instances of input1 into array.
         });
     });
-//Output display & execute button
+// operators function to calculate.
+let operatorInput = document.querySelectorAll(".ope");
+    operatorInput.forEach(operator => {
+        operator.addEventListener('click', (event) => {// only return input1 once an operator is clicked.
+            operatorInput = event.target.innerHTML; // access the symbols.
+            displayOp = document.createTextNode(operatorInput);
+            topScreen.appendChild(displayOp);
+            jointInput1 = displayArray.join('');// join elements inside array into string.
+            console.log('first input: '+jointInput1);
+            input1 = parseFloat(jointInput1);// assign joint string into input1 parsed as integer.
+            console.log(operatorInput);
+            displayArray = []; // empty array after operator clicked, ready to take value of input 2.
+        });
+    });
+// output display & execute button
 const bottomScreen = document.querySelector(".screenBottom");
 const outputEqual = document.querySelector(".equ");
     outputEqual.addEventListener('click', (event) => {
+            //clear all inputs
+            let clearInput = document.querySelector(".screenTop");
+                while (clearInput.firstChild) {
+                    clearInput.removeChild(clearInput.firstChild);
+                }        
         jointInput2 = displayArray.join('');
         console.log('second input: '+jointInput2);
-        input2 = parseInt(jointInput2);
+        input2 = parseFloat(jointInput2);
         //conditions for calculations based on what operator were clicked.
             if (operatorInput == userInput.operators[0]) {
                 let sum = input1 + input2;
-                equationResult = sum;
-                console.log('equal: ' +sum);
+                equationResult = Math.round(sum * 1000000) / 1000000;
             }
             else if (operatorInput == userInput.operators[1]) {
                 let subtraction = input1 - input2;
-                equationResult = subtraction;
-                console.log('equal: ' +subtraction);
+                equationResult = Math.round(subtraction * 1000000) / 1000000;
             }
             else if (operatorInput == userInput.operators[2]) {
                 let multiply = input1 * input2;
-                equationResult = multiply;
-                console.log('equal: ' +multiply);
+                equationResult = Math.round(multiply * 1000000) / 1000000;
             }
             else if (operatorInput == userInput.operators[3]) {
                 let division = input1 / input2;
@@ -70,26 +71,26 @@ const outputEqual = document.querySelector(".equ");
         bottomScreen.innerHTML = equationResult;
         displayArray = [];
     });
-//Reset (AC)
+// reset (AC)
 const allClear = document.querySelector(".clearBtn");
         allClear.addEventListener('click', (clear) => {
-            //Clear both screens
+            // clear both screens
             let clearScreenTop = document.querySelector(".screenTop");
             while (clearScreenTop.firstChild) {
                 clearScreenTop.removeChild(clearScreenTop.firstChild);
             }
             let clearScreenBottom = document.querySelector(".screenBottom");
             clearScreenBottom.removeChild(clearScreenBottom.firstChild);
-            //clear all input, outoput values
+            // clear all input & output values
             input1 = null;
             input2 = null;
             displayArray = [];
             equationResult = null;
-        });
-//Delete (C)
+            });
+// delete (C)
 const deleteInput = document.querySelector(".deleteBtn");
         deleteInput.addEventListener('click', (remove) =>{
             let removeChar = document.querySelector(".screenTop");
             removeChar.removeChild(removeChar.lastChild);
-            displayArray.pop();//remove last element in display array when click.
-        })
+            displayArray.pop();// remove last element in display array when clicked C.
+            })
